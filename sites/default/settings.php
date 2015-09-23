6d0e92421763b097bc9cb4feffca3bf949e82af0
petitrole <celinegarcher@gmail.com>
2015-09-23 12:19:44 +0200

    settings

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -217,7 +217,7 @@ $databases = array (
     array (
       'database' => 'osteo',
       'username' => 'root',
-      'password' => '',
+      'password' => 'azerty77',
       'host' => 'localhost',
       'port' => '',
       'driver' => 'mysql',
